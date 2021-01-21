d8b25d3d64923ad444d8c6a00b04b463ee07b2c2
Jaydar <Jaydar@Outlook.com>
2021-01-21 18:16:58 +0800

    up

--- src/Code/action/Setting.ts
+++ src/Code/action/Setting.ts
@@ -1,17 +1,63 @@
-import * as vscode from 'vscode';
+import { settings } from 'cluster';
+import * as vs from 'vscode';
+
+interface iSettingsItem extends vs.QuickPickItem{
+    event():void;
+}
 
 export default class Settings {
 
-    constructor(context: vscode.ExtensionContext) {
+
+    static context:vs.ExtensionContext
+
+    static init(context: vs.ExtensionContext){
+        Settings.context = context;
+    }
+
+
+    static sync() {
+        vs.window.showInformationMessage('settings.sync');
+    }
+
+    static async showOptin() {
         
+        const items: iSettingsItem[] = [
+            {
+                label: 'Sign in',
+                description:'Sign in to GitHub',
+                event:this.signIn
+            },
+            {
+                label: 'Sign up',
+                description:'Create your account',
+                event:this.signUp
+            },
+        ]
+        const quickPick = vs.window.createQuickPick();
+        quickPick.items = items;
+        quickPick.onDidChangeSelection((selection) => {
+           let option = selection[0] as iSettingsItem;
+           option.event()
+        });
+        quickPick.onDidHide(() => {
+            quickPick.dispose()
+        });
+        quickPick.show();
     }
 
-
-    static sync(){
-        vscode.window.showInformationMessage('settings.sync');
+    static async signIn(){
+        Settings.context.globalState.update('user',{
+            name:"name",
+            pass:"pass"
+        })
     }
 
-    static showOptin() {
-        vscode.window.showInformationMessage('settings.showOptin');
+    static async signOut(){
+        console.log('signOut')
+    }
+
+    static async signUp(){
+
     }
 }
+
--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,21 @@
 
 import * as vscode from 'vscode';
 import { SettingsTDO } from './Code/view/main';
-import Account from './Code/action/account';
+import Account from './Code/action/Account';
 import Settings from './Code/action/Setting';
 
 
 export function activate(context: vscode.ExtensionContext) {
 
-
-	
-	// const settingsTDP = new SettingsTDO();
-
 	context.subscriptions.push(
 		vscode.commands.registerCommand('vs-note.dico', () => {
 			vscode.window.showInformationMessage('Hello World from VS Note!');
-		}),
-		vscode.commands.registerCommand('account.login', () => Account.login()),
-		vscode.commands.registerCommand('account.logout', () => Account.login()),
-		vscode.commands.registerCommand('account.sync',()=> Settings.showOptin())
+		})
 	);
-	
-	
-	
-	
-	// let account = new Account(context);
-	// vscode.commands.registerCommand('account.login', account.login);
-	// vscode.commands.registerCommand('account.logout', account.logout);
-	// vscode.commands.registerCommand('account.sync', account.sync);
-
-
-	// let settings = new Settings(context);
-	// vscode.commands.registerCommand('settings.showOptin', settings.showOptin);
-
-
-
+	vscode.commands.registerCommand('account.login', () => Account.login()),
+	vscode.commands.registerCommand('account.logout', () => Account.login()),
+	vscode.commands.registerCommand('settings.showOption',()=> Settings.showOptin()),
+	vscode.commands.registerCommand('settings.sync',()=> Settings.sync())
 }
 
 // this method is called when your extension is deactivated
